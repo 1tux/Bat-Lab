added softmax model. fixed HD plot. added test over different timebins for neuron72

analysis_lib.py
@@ -21,7 +21,7 @@ import feature_engineering
 import parse_real_neural_data
 import uuid
 
-from models import SVMModel
+from models import SVMModel, SoftMAXModel
 from constants import *
 import config
 
@@ -341,6 +341,7 @@ def cell_analysis(df, neuron, neuron_description=""):
         print("CLEANING NOISE")
         neuron = SVM_utils.noise_cancellation(neuron)
 
+    print("After cleaning there are:", neuron.sum(), "spikes")
     if neuron.sum() < config.Config.get("MIN_SPIKES"):
         print(f'Too few spikes (< conf:{config.Config.get("MIN_SPIKES")})')
         return True
@@ -359,11 +360,12 @@ def cell_analysis(df, neuron, neuron_description=""):
     df_ = df.reset_index(drop=True)
 
     # print(df_.head())
-
+    
     shuffled_vals = []
     threads = []
 
     neuron_dropped = neuron[df.index].reset_index(drop=True)
+    print("After removing NaNs from dataframe:", neuron_dropped.sum(), "spikes")
 
     if neuron_dropped.sum() < config.Config.get("MIN_SPIKES"):
         print(f'Too few spikes (< conf:{config.Config.get("MIN_SPIKES")})')
@@ -380,7 +382,15 @@ def cell_analysis(df, neuron, neuron_description=""):
 
     for xid, shuffled_neuron in enumerate(shuffling.shuffling(neuron_dropped, config.Config.get("N_SHUFFLES"))):
         shuffled_neuron = shuffled_neuron.astype('int')
-        model = SVMModel(multi_threaded=True, upsample=config.Config.get('UPSAMPLING'))
+        model_class_str = config.Config.get("MODEL")
+        if model_class_str == "SVM":
+            model_class = SVMModel
+        elif model_class_str == "SoftMAX":
+            model_class = SoftMAXModel
+        else:
+            raise Exception("UNKNOWN MODEL in CONFIGURATION FILE")
+
+        model = model_class(multi_threaded=True, upsample=config.Config.get('UPSAMPLING'))
 
         if not config.Config.get('UPSAMPLING'):
             WEIGHT = dict(zip([0,1], len(shuffled_neuron) / (2*np.bincount(shuffled_neuron))))
@@ -407,7 +417,7 @@ def cell_analysis(df, neuron, neuron_description=""):
         return True
 
     # model = SVM_model(cv=CV)
-    model = SVMModel(cv=config.Config.get("CV"), multi_threaded=True, upsample=config.Config.get('UPSAMPLING'))
+    model = model_class(cv=config.Config.get("CV"), multi_threaded=True, upsample=config.Config.get('UPSAMPLING'))
     if not config.Config.get('UPSAMPLING'):
         WEIGHT = dict(zip([0,1], len(neuron) / ( 2 * np.bincount(neuron) )))
         if config.Config.get("SQRT_WEIGHT"):

models.py
@@ -1,4 +1,4 @@
-from sklearn.linear_model import SGDClassifier
+from sklearn.linear_model import SGDClassifier, LogisticRegression
 from sklearn.model_selection import train_test_split, ShuffleSplit, KFold, StratifiedKFold
 import importance
 from sklearn.metrics import confusion_matrix
@@ -8,7 +8,7 @@ from constants import *
 import SVM_utils
 # from xgboost import XGBClassifier
 import copy
-
+from math import sqrt
 
 def per_cross_validation(model, X_train, X_test, y_train, y_test):
     if model.upsample:
@@ -16,6 +16,7 @@ def per_cross_validation(model, X_train, X_test, y_train, y_test):
     model.train(X_train, y_train)
 
     model.train_cm = confusion_matrix(y_train, model.predict(X_train))
+    print("predicting:", model.predict(X_test).sum(), "out of", y_test.sum(), "spikes")
     model.test_cm = confusion_matrix(y_test, model.predict(X_test))
 
     return model, model.train_cm, model.test_cm
@@ -124,6 +125,34 @@ class Model:
         # TODO: return self instead, or actually not return anything at all!
         return svm_models[0], train_cms, test_cms, imp_table, agg_imp_table, std_train_cm, std_test_cm
 
+    def evaluate(confusion_matrix):
+        # calculating MCC score: https://en.wikipedia.org/wiki/Matthews_correlation_coefficient
+
+        tp = confusion_matrix[1][1]
+        tn = confusion_matrix[1][0]
+        fp = confusion_matrix[0][1]
+        fn = confusion_matrix[0][0]
+
+        mcc = tp * tn - fp*fn / sqrt((tp + fp)*(tp+fn)*(tn+fp)*(tn+fn))
+        return mcc
+
+    def evaluate_against_shuffles(model_cms, shuffles_cms):
+        # assuming we got several confusion_matrices from running the model with CV.
+        # assuming we got several confusion_matrices from shuffles.
+        # we return the difference between the medians of the MCCs of the confusion_matrices.
+
+        models_mcc = []
+        for cm in models_cms:
+            models_mcc.append(evaluate(cm))
+
+        shuffles_mcc = []
+        for cm in shuffles_cms:
+            shuffles_mcc.append(evaluate(cm))
+
+        mcc1 = np.median(models_mcc)
+        mcc2 = np.median(shuffles_mcc)
+
+        return mcc1 - mcc2
 
 class SVMModel(Model):
 
@@ -141,6 +170,53 @@ class SVMModel(Model):
             self.model = SGDClassifier(power_t=0.4, n_jobs=1, max_iter=10 ** 5, learning_rate="invscaling", eta0=1,
                                        penalty='L1', n_iter_no_change=10, random_state=1337)
 
+    def train(self, X, y):
+        self.X_train = X
+        self.y_train = y
+        self.model.fit(X, y)
+
+    def predict(self, X_test):
+        self.X_test = X_test
+        self.X_preds = self.model.predict(X_test)
+        return self.X_preds
+
+    def get_importances(self):
+        return np.abs(self.model.coef_[0])
+
+    def __call__(self, X, y):
+        if self.cv:
+            return self.cross_validate(X, y)
+        else:
+            return self.single_run(X, y)
+
+    def set_weight(self, weight):
+        self.model.class_weight = weight
+
+    def get_all_importances(self):
+        assert self.svm_models is not None, "you can't store all CV without running CV"
+        coeffs = []
+        for m in self.svm_models:
+            coeffs.append(m.model.coef_[0])
+        return coeffs
+
+class SoftMAXModel(Model):
+
+    def __init__(self, model=None, cv=False, multi_threaded=True, upsample=True):
+        self.X_train = None
+        self.y_train = None
+        self.X_test = None
+        self.X_preds = None
+
+        self.upsample = upsample
+        self.model = model
+        self.cv = (cv > 1) * cv
+        self.multi_threaded = multi_threaded
+        if model is None:
+            #self.model = LogisticRegression(n_jobs=1, penalty='l1', random_state=1337, max_iter=10 ** 3, solver='saga')
+            self.model = SGDClassifier(loss='log', power_t=0.4, n_jobs=1, max_iter=10 ** 5, learning_rate="invscaling", eta0=1,
+                                       penalty='L1', n_iter_no_change=10, random_state=1337)
+            print("Softmax!")
+            
     def train(self, X, y):
         self.X_train = X
         self.y_train = y

spike_plots.py
@@ -266,6 +266,18 @@ def hd_plot_1d(df, neuron, model_spikes, ax=None):
 
     hd_spikes_radians = df_neuron[dataset.get_col_name(0, "HD")] / 180 * np.pi
     ax.set_yticks([])
-    ax.hist(df[dataset.get_col_name(0, "HD")] / 180 * np.pi, bins=36, color="#cccccc", density=True)
-    ax.hist(hd_spikes_radians, bins=36, color="red", density=True)
-    ax.hist(df_model_spikes[dataset.get_col_name(0, "HD")] / 180 * np.pi, bins=36, color="blue", density=True)
+    behavior = df[dataset.get_col_name(0, "HD")] / 180 * np.pi
+    behavior_map = np.histogram(behavior, bins=np.linspace(0, 1, 36) * 2*np.pi)[0]
+    hd_spikes_radians_map = np.histogram(hd_spikes_radians, bins=np.linspace(0, 1, 36) * 2*np.pi)[0]
+    model_spikes = df_model_spikes[dataset.get_col_name(0, "HD")] / 180 * np.pi
+    model_spikes_map = np.histogram(model_spikes, bins=np.linspace(0, 1, 36) * 2*np.pi)[0]
+
+    print(hd_spikes_radians_map)
+    print(behavior_map)
+    print(hd_spikes_radians_map / behavior_map)
+    print(model_spikes_map / behavior_map)
+    #ax.hist(behavior_map, bins=36, color="#cccccc", density=True)
+    #ax.hist(hd_spikes_radians, bins=36, color="red", density=True)
+    ax.plot(hd_spikes_radians_map / behavior_map)
+    ax.plot(model_spikes_map / behavior_map)
+    #ax.hist(df_model_spikes[dataset.get_col_name(0, "HD")] / 180 * np.pi, bins=36, color="blue", density=True, alpha=0.2)